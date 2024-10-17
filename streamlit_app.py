8206c87f8d4e2b840f4e24e000d946d16d2f4692
Nannnnnnnnnnnn <135064881+Nannnnnnnnnnnn@users.noreply.github.com>
2024-10-17 14:12:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,15 @@ no_indicator_calculation = st.Page("no_indicator_calculation.py", title="No Indi
 with_indicator_calculation = st.Page("with_indicator_calculation.py", title="With Indicator Calculation", icon=":material/edit:")
 
 pg = st.navigation([no_indicator_calculation, with_indicator_calculation])
-st.set_page_config(page_title="Order Shaping Tool", page_icon=":milky_way:")
+st.set_page_config(
+    page_title="Order Shaping Tool",
+    page_icon=":milky_way:",
+    layout="wide",
+    menu_items={
+        # "Get Help": "https://www.extremelycoolapp.com/help",
+        "Report a bug": "mailto:wang.n.22@pg.com",
+        "About": """Developer: Wang Nan
+                    \n Email: wang.n.22@pg.com"""
+    }
+)
 pg.run()
