0946d7da7d1d6436e81dc7a69715a903dbb9afe2
Nannnnnnnnnnnn <135064881+Nannnnnnnnnnnn@users.noreply.github.com>
2024-11-18 10:26:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ no_indicator_calculation = st.Page("no_indicator_calculation.py", title="No Indi
 with_indicator_calculation = st.Page("with_indicator_calculation.py", title="With Indicator Calculation", icon=":material/edit:")
 
 pg = st.navigation([no_indicator_calculation, with_indicator_calculation])
-st.set_page_config(page_title="Order Shaping Tool", page_icon=":milky_way:")
 st.set_page_config(
     page_title="Order Shaping Tool",
     page_icon=":milky_way:",
