7f6a6dad5a4ae4aaaa5fbc7e1916804e6b55cb2f
Nannnnnnnnnnnn <135064881+Nannnnnnnnnnnn@users.noreply.github.com>
2024-11-25 13:53:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,9 @@ import streamlit as st
 no_indicator_calculation = st.Page("no_indicator_calculation.py", title="No Indicator Calculation", icon=":material/edit:")
 with_indicator_calculation = st.Page("with_indicator_calculation.py", title="With Indicator Calculation", icon=":material/edit:")
 qty_and_price_limit_calculation = st.Page("qty_and_price_limit_calculation.py", title="Qty and Price Limit Calculation", icon=":material/edit:")
+version_converting = st.Page("version_converting_tool.py", title="Version Converting", icon=":material/edit:")
 
-pg = st.navigation([no_indicator_calculation, with_indicator_calculation, qty_and_price_limit_calculation])
+pg = st.navigation([no_indicator_calculation, with_indicator_calculation, qty_and_price_limit_calculation, version_converting])
 st.set_page_config(
     page_title="Order Shaping Tool",
     page_icon=":milky_way:",
